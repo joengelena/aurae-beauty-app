541f2a8f264365cb7cd2efee1eff0af9df8c26c9
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-06-23 17:34:31 +1200

    Added the watchlist page

--- lib/presentation/pages/watchlist_page.dart
+++ lib/presentation/pages/watchlist_page.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:motorix_app/presentation/widgets/listing/watchlist_preview.dart';
+import 'package:go_router/go_router.dart';
 
 class WatchlistPage extends StatelessWidget {
   const WatchlistPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Text('Watchlist Page');
+    return ListView(
+      children: [
+        WatchlistPreview(),
+        WatchlistPreview(),
+        Align(
+          alignment: Alignment.center,
+          child: FilledButton(
+            onPressed: () {
+              context.go('/listings');
+            },
+            child: Text('Explore more'),
+          ),
+        ),
+      ],
+    );
   }
 }
