3f277b1d2546543bde228be1fb5023d52ff399b0
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-06-25 19:22:22 +1200

    Move app navigation into a seperate file

---
+++ lib/presentation/widgets/scaffold/app_navigation.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class AppNavigation extends StatelessWidget {
+  final GoRouterState state;
+
+  const AppNavigation({super.key, required this.state});
+
+  int _calculateIndex(String uri) {
+    if (uri.startsWith('/watchlist')) return 1;
+    if (uri.startsWith('/profile')) return 2;
+    return 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        Center(
+          child: SizedBox(
+            width: 400,
+            child: NavigationBar(
+              onDestinationSelected: (index) {
+                switch (index) {
+                  case 0:
+                    context.go('/listings');
+                    break;
+                  case 1:
+                    context.go('/watchlist');
+                    break;
+                  case 2:
+                    context.go('/profile');
+                    break;
+                }
+              },
+              selectedIndex: _calculateIndex(state.uri.toString()),
+              indicatorColor: Colors.grey.shade400,
+              backgroundColor: Colors.white,
+              height: 70,
+              destinations: const [
+                NavigationDestination(
+                  icon: Icon(Icons.search),
+                  label: 'Explore',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.bookmark),
+                  label: 'Watchlist',
+                ),
+                NavigationDestination(
+                  icon: Icon(Icons.person),
+                  label: 'Profile',
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/presentation/widgets/scaffold/app_scaffold.dart
+++ lib/presentation/widgets/scaffold/app_scaffold.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:motorix_app/presentation/widgets/scaffold/app_navigation.dart';
 import 'package:motorix_app/presentation/widgets/scaffold/title_app_bar.dart';
 
 class AppScaffold extends StatelessWidget {
@@ -8,60 +9,12 @@ class AppScaffold extends StatelessWidget {
 
   const AppScaffold({super.key, required this.state, required this.child});
 
-  int _calculateIndex(String uri) {
-    if (uri.startsWith('/watchlist')) return 1;
-    if (uri.startsWith('/profile')) return 2;
-    return 0;
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: TitleAppBar(currentRoute: state.uri.toString()),
       body: child,
-      bottomNavigationBar: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Center(
-            child: SizedBox(
-              width: 400,
-              child: NavigationBar(
-                onDestinationSelected: (index) {
-                  switch (index) {
-                    case 0:
-                      context.go('/listings');
-                      break;
-                    case 1:
-                      context.go('/watchlist');
-                      break;
-                    case 2:
-                      context.go('/profile');
-                      break;
-                  }
-                },
-                selectedIndex: _calculateIndex(state.uri.toString()),
-                indicatorColor: Colors.grey.shade400,
-                backgroundColor: Colors.white,
-                height: 70,
-                destinations: const [
-                  NavigationDestination(
-                    icon: Icon(Icons.search),
-                    label: 'Explore',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.bookmark),
-                    label: 'Watchlist',
-                  ),
-                  NavigationDestination(
-                    icon: Icon(Icons.person),
-                    label: 'Profile',
-                  ),
-                ],
-              ),
-            ),
-          ),
-        ],
-      ),
+      bottomNavigationBar: AppNavigation(state: state),
     );
   }
 }
