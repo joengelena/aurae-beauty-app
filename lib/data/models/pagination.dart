406d4353193355268ea16d5f1e5ab20c999d81b6
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-07-05 10:42:30 +1200

    Create a pagination reponse class for listings repsonse from the api

---
+++ lib/data/models/pagination.dart
@@ -0,0 +1,30 @@
+class PaginatedResponse<T> {
+  final List<T> data;
+  final int currentPage;
+  final int totalPages;
+  final int totalRows;
+
+  PaginatedResponse({
+    required this.data,
+    required this.currentPage,
+    required this.totalPages,
+    required this.totalRows,
+  });
+
+  factory PaginatedResponse.fromJson(
+    Map<String, dynamic> json,
+    T Function(Map<String, dynamic>) fromJsonT,
+  ) {
+    final items =
+        (json['data'] as List)
+            .map((e) => fromJsonT(e as Map<String, dynamic>))
+            .toList();
+
+    return PaginatedResponse<T>(
+      data: items,
+      currentPage: json['currentPage'] as int,
+      totalPages: json['totalPages'] as int,
+      totalRows: json['totalRows'] as int,
+    );
+  }
+}
