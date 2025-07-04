33f54e4b7b2637fe20f69c1028e9f55cb149db2a
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-07-05 10:41:43 +1200

    Update api client get function to be able to be passed query params

--- lib/data/api_client.dart
+++ lib/data/api_client.dart
@@ -6,12 +6,21 @@ class ApiClient {
   final String _baseUrl = 'http://localhost:4941/api/v1';
   static final http.Client _client = http.Client();
 
-  Future<http.Response> get(String path) async {
-    String? authToken = await SecureStorage.read('authToken');
-    String? csrfToken = await SecureStorage.read('csrfToken');
+  Future<http.Response> get(
+    String path, {
+    Map<String, dynamic>? queryParameters,
+  }) async {
+    final String? authToken = await SecureStorage.read('authToken');
+    final String? csrfToken = await SecureStorage.read('csrfToken');
+
+    final uri = Uri.parse('$_baseUrl$path').replace(
+      queryParameters: queryParameters?.map(
+        (key, value) => MapEntry(key, value.toString()),
+      ),
+    );
 
     final response = await _client.get(
-      Uri.parse('$_baseUrl$path'),
+      uri,
       headers: {
         'Content-Type': 'application/json',
         'mtx-auth-token': authToken ?? '',
