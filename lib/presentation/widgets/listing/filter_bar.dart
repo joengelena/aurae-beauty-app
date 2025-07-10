64eb212794f484040c9e569c68dca122ddf3f933
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-07-10 21:36:45 +1200

    Create a seperate filter bar widget that utilizes the filters in the listings provider

--- lib/presentation/widgets/listing/filter_bar.dart
+++ lib/presentation/widgets/listing/filter_bar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:motorix_app/data/models/listing_filter.dart';
+import 'package:motorix_app/logic/listings_provider.dart';
+import 'package:provider/provider.dart';
 
 class FilterBar extends StatelessWidget {
   const FilterBar({super.key});
@@ -7,8 +10,51 @@ class FilterBar extends StatelessWidget {
     return FilterChip(label: Text(label), selected: true, onSelected: (_) {});
   }
 
+  void showFiltersBottomSheet(
+    BuildContext context,
+    List<ListingFilter> filters,
+  ) {
+    showModalBottomSheet(
+      context: context,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(16)),
+      ),
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children:
+                filters
+                    .map(
+                      (filter) => Row(
+                        children: [
+                          Text(filter.name),
+                          DropdownButton<String>(
+                            items:
+                                filter.filterValues.map((val) {
+                                  return DropdownMenuItem(
+                                    value: val,
+                                    child: Text(val),
+                                  );
+                                }).toList(),
+                            onChanged: (value) {},
+                          ),
+                        ],
+                      ),
+                    )
+                    .toList(),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    final provider = context.watch<ListingsProvider>();
+    final filters = provider.filters;
+
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       padding: EdgeInsets.symmetric(horizontal: 16),
@@ -33,7 +79,9 @@ class FilterBar extends StatelessWidget {
               'Filters',
               style: TextStyle(color: Theme.of(context).primaryColor),
             ),
-            onPressed: () {},
+            onPressed: () {
+              showFiltersBottomSheet(context, filters);
+            },
           ),
           _buildChip("Used"),
           _buildChip("2WD"),
