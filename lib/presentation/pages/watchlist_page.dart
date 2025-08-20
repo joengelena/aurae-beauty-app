59bbbce192eed558b4add41ba4ee93141208f4e8
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-08-20 09:53:36 +0900

    The watchlist page will no longer be available to users that are not signed in and  the user will be prompted to sign in the view their watchlist

--- lib/presentation/pages/watchlist_page.dart
+++ lib/presentation/pages/watchlist_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:motorix_app/presentation/widgets/listing/listing_tile.dart';
+import 'package:motorix_app/logic/auth_provider.dart';
+import 'package:motorix_app/presentation/widgets/sign_in_to_access.dart';
 import 'package:go_router/go_router.dart';
 
 class WatchlistPage extends StatelessWidget {
@@ -7,44 +9,58 @@ class WatchlistPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        constraints: BoxConstraints(maxWidth: 600),
-        child: ListView(
-          children: [
-            SizedBox(height: 12),
-            ListingTile(
-              topRightButtom: IconButton(
-                onPressed: () {
-                  // handle delete action
-                },
-                icon: const Icon(Icons.delete_outline),
-                color: Colors.red,
+    final authProvider = AuthProvider();
+    return FutureBuilder<bool>(
+      future: authProvider.isSignedIn(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasData && snapshot.data == true) {
+          return Center(
+            child: Container(
+              constraints: BoxConstraints(maxWidth: 600),
+              child: ListView(
+                children: [
+                  SizedBox(height: 12),
+                  ListingTile(
+                    topRightButtom: IconButton(
+                      onPressed: () {
+                        // handle delete action
+                      },
+                      icon: const Icon(Icons.delete_outline),
+                      color: Colors.red,
+                    ),
+                  ),
+                  ListingTile(
+                    topRightButtom: IconButton(
+                      onPressed: () {
+                        // handle delete action
+                      },
+                      icon: const Icon(Icons.delete_outline),
+                      color: Colors.red,
+                    ),
+                  ),
+                  SizedBox(height: 24),
+                  Align(
+                    alignment: Alignment.center,
+                    child: FilledButton(
+                      onPressed: () {
+                        context.go('/listings');
+                      },
+                      child: Text('Explore more'),
+                    ),
+                  ),
+                ],
               ),
             ),
-            ListingTile(
-              topRightButtom: IconButton(
-                onPressed: () {
-                  // handle delete action
-                },
-                icon: const Icon(Icons.delete_outline),
-                color: Colors.red,
-              ),
-            ),
-            SizedBox(height: 24),
-
-            Align(
-              alignment: Alignment.center,
-              child: FilledButton(
-                onPressed: () {
-                  context.go('/listings');
-                },
-                child: Text('Explore more'),
-              ),
-            ),
-          ],
-        ),
-      ),
+          );
+        } else {
+          return const SignInToAccess(
+            message: 'Sign in to view your watchlist.',
+          );
+        }
+      },
     );
   }
 }
