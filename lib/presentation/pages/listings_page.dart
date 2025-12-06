8b34e38180c75ce690b121a175a4bfa3d744f7cd
Bruhdus <41399077+Bruhdus@users.noreply.github.com>
2025-12-06 16:39:00 +1300

    Create a floating action button that navigates to post listing page from the listings page

--- lib/presentation/pages/listings_page.dart
+++ lib/presentation/pages/listings_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:motorix_app/presentation/widgets/listing/infinite_grid.dart';
 import 'package:motorix_app/presentation/widgets/listing/search_and_filter_bar.dart';
 
@@ -7,8 +8,20 @@ class ListingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [SearchAndFiltersBar(), Expanded(child: InfiniteGrid())],
+    return Stack(
+      children: [
+        Column(
+          children: [SearchAndFiltersBar(), Expanded(child: InfiniteGrid())],
+        ),
+        Positioned(
+          right: 16,
+          bottom: 16,
+          child: FloatingActionButton(
+            onPressed: () => context.go('/post-listing'),
+            child: Icon(Icons.add),
+          ),
+        ),
+      ],
     );
   }
 }
